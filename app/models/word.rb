a3f3b3988e46149f5cfc2d2209b60508d4b7691f
Richard Huang <flyerhzm@gmail.com>
2013-02-16 20:30:14 +0800

    parse words.dic

---
+++ app/models/word.rb
@@ -0,0 +1,15 @@
+class Word
+  class <<self
+    def add(word)
+      $redis.sadd key, word
+    end
+
+    def exist?(word)
+      $redis.sismember key, word
+    end
+
+    def key
+      "words"
+    end
+  end
+end
--- lib/dicts/parser.rb
+++ lib/dicts/parser.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
-require "dicts/parser/modern_chinese_dict_parser"
-require "dicts/parser/synonyms_dict_parser"
-require "dicts/parser/stopwords_parser"
 require "dicts/parser/chinese_pinyin_parser"
+require "dicts/parser/modern_chinese_dict_parser"
+require "dicts/parser/stopwords_parser"
+require "dicts/parser/synonyms_dict_parser"
+require "dicts/parser/words_dict_parser"
