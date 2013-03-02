34ce387c991fc32ac8adcc3c854bc6a2225d2846
Richard Huang <flyerhzm@gmail.com>
2013-03-02 18:16:40 +0800

    remove unused files

--- config/app.rb
+++
@@ -1,3 +0,0 @@
-$redis = EventMachine::Synchrony::ConnectionPool.new(size: 2) do
-  Redis::Namespace.new($redis_namespace, redis: Redis.new(host: $redis_host, port: $redis_prot, driver: "synchrony"))
-end
--- lib/dicts/parser.rb
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-require "dicts/parser/chinese_pinyin_parser"
-require "dicts/parser/modern_chinese_dict_parser"
-require "dicts/parser/stopwords_parser"
-require "dicts/parser/synonyms_dict_parser"
-require "dicts/parser/words_dict_parser"
-require "dicts/parser/hypernyms_dict_parser"
