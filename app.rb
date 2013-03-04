e186610072043f86743f1a7741fceb67b37a0d4a
Richard Huang <flyerhzm@gmail.com>
2013-03-05 00:29:18 +0800

    automatically get port from echidna-env

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ Bundler.require(:default, echidna_env)
 require "goliath"
 
 dicts_ip = ENV['ECHIDNA_DICTS_IP'] || "0.0.0.0"
-dicts_port = ENV['ECHIDNA_DICTS_PORT']
+dicts_port = app_port('dicts')
 dicts_daemon = ENV['ECHIDNA_DICTS_DAEMON'] == 'true'
 
 ARGV.replace ["-e", echidna_env, "-a", dicts_ip, "-p", dicts_port]
