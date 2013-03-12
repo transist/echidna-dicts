6a971a19b4cfc9fbc4163f106ac64908511482a1
Richard Huang <flyerhzm@gmail.com>
2013-03-12 19:10:30 +0800

    add english stopwrods

--- lib/dicts/parser/stopwords_parser.rb
+++ lib/dicts/parser/stopwords_parser.rb
@@ -2,12 +2,14 @@
 module Dicts
   module Parser
     class StopwordsParser
-      DICT_FILENAME = 'dicts/chinese_stopwords.txt'
+      DICT_FILENAMES= %w(dicts/chinese_stopwords.txt dicts/english_stopwords.txt)
 
       def parse
-        File.open(DICT_FILENAME, 'r') do |file|
-          file.each_line do |line|
-            Stopword.add(line.strip)
+        DICT_FILENAMES.each do |filename|
+          File.open(filename, 'r') do |file|
+            file.each_line do |line|
+              Stopword.add(line.strip)
+            end
           end
         end
       end
